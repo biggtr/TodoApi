61caedc955134414fd56d031781a91e46f5268c4
bigtorresg <bigtorresg@gmail.com>
2023-10-23 19:44:39 +0300

    password reset confirm fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include, re_path
-
+from dj_rest_auth.views import PasswordResetConfirmView
 from accounts.views import CustomEmailConfirmView
 
 
 urlpatterns = [
     path("", include("dj_rest_auth.urls")),
+    path(
+        "password/reset/confirm/<str:uidb64>/<str:token>",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
     re_path(
         r"^registration/account-confirm-email/(?P<key>[-:\w]+)/$",
         CustomEmailConfirmView.as_view(),
