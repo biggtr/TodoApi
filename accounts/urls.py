6927a68e3dbaaf33c384d0de945f1f13be09f0cb
bigtorresg <bigtorresg@gmail.com>
2023-10-23 18:45:27 +0300

    verify email is added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from accounts.views import CustomEmailConfirmView
 
 
 urlpatterns = [
     path("", include("dj_rest_auth.urls")),
+    re_path(
+        r"^registration/account-confirm-email/(?P<key>[-:\w]+)/$",
+        CustomEmailConfirmView.as_view(),
+        name="account_confirm_email",
+    ),
     path("registration/", include("dj_rest_auth.registration.urls")),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# views.py
+import requests
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from allauth.account.models import EmailConfirmation
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class CustomEmailConfirmView(APIView):
+    def get(self, request, key):
+        verify_email_url = "http://localhost:8000/api-auth/registration/verify-email/"
+
+        # make a POST request to the verify-email endpoint with the key
+        response = requests.post(verify_email_url, {"key": key})
+        if response.status_code == 200:
+            return Response(
+                {"message": "Email verified successfully"}, status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"message": "Email verification failed"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
